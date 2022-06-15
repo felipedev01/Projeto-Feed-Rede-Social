e707198371d03d0f74e445192d583d2570491f19
Felipe <felipecss88@gmail.com>
2022-06-15 17:01:59 -0300

    Logo implementado

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,13 +1,14 @@
 import styles from'./Header.module.css'
+import igniteLogo from '../Assets/Ignite-logo.svg'
 
-console.log(styles)
+console.log(igniteLogo)
 
 export function Header(){
 
     return(
     <div>
       <header className={styles.Header}> 
-        <strong>Ignite Feed</strong>
+        <img src="/src/Assets/Ignite-logo.svg" alt="Logo Ignite" />
         </header>
     </div>
         
