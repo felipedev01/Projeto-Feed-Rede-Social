60ee62cf844ef65757b7cdb3c4ce1b254ba49d14
Felipe <felipecss88@gmail.com>
2022-06-16 15:17:22 -0300

    Sidebar footer estilizado

--- src/Componentes/Sidebar.jsx
+++ src/Componentes/Sidebar.jsx
@@ -9,15 +9,17 @@ export function Sidebar(){
             <img className={Styles.cover} src="https://images.unsplash.com/photo-1615921983172-638995cbd490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTF8YkNVNmtfS1B1dVl8fGVufDB8fHx8&auto=format&fit=crop&w=259&q=60" 
             alt="" />
             <div className={Styles.profile}>
+                <img className={Styles.image} src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=60&q=60" 
+                alt="" />
                 <strong>Felipe Cristovão</strong>
                 <span>Web developer</span>
-                <footer>
+                
+            </div>
+            <footer>
                     <a href="#">
                         Editar seu perfil
                     </a>
                 </footer>
-            </div>
-
           
         </aside>
     )
