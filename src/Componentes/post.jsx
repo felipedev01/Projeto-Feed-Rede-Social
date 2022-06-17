eab33bb31d7c7fb27d2ac9cad91a5b562cffd24e
Felipe <felipecss88@gmail.com>
2022-06-16 23:47:55 -0300

    Componente header finalizado

--- src/Componentes/post.jsx
+++ src/Componentes/post.jsx
@@ -6,15 +6,28 @@
 
     return (
         <article className={styles.post}>
-          <header>
-        
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/81122606?v=" alt="" />
-            
-          </header>
-          <div>
-            <strong>Felipe Cristovão</strong>
-            <span>Desenvolvedor Web</span>
-          </div>
+
+           <header>
+            <div className={styles.author}>
+            <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/81122606?v=" alt="" ></img>
+            <div className={styles.authorinfo}>
+              <strong>Felipe Cristovão</strong>
+              <span>Web Developer</span>
+            </div>
+            </div>
+            <time title='16 de junho' dateTime='2022-06-16'>Publicado à 1 hora</time>
+           </header>
+
+           <div className={styles.content}>
+            Fala galeraa 👋
+
+            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+
+           👉 jane.design/doctorcare
+
+            #novoprojeto #nlw #rocketseat
+           </div>
+               
         </article>
         
     )
