0cfadeae071c49dc72947d7ec823123179f3f72e
Felipe <felipecss88@gmail.com>
2022-06-18 10:36:28 -0300

    Atualizando componente comment

--- src/Componentes/Comment.jsx
+++ src/Componentes/Comment.jsx
@@ -1,3 +1,4 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 export function Comment(){
 
@@ -7,10 +8,26 @@ export function Comment(){
             <img src="https://avatars.githubusercontent.com/u/81122606?v=" alt="" />
             <div className={styles.commentBox}>
              <div className={styles.commentContent}>
-             Comentario
+             <header>
+              <div className={styles.authorEndTime}>
+              <strong>Felipe Cristovão</strong>
+
+                <time title='16 de junho' dateTime='2022-06-16'>Cerca de há 1h atrás</time>
+
+              </div>
+                
+                <button title='Botão deletar'>
+                    <Trash size={20}></Trash>
+                </button>
+
+             </header>
+
+             <p>Muito bom Devon, parabéns!! 👏👏</p>
              </div>
           <footer>
-            Aplaudir
+           <button>
+            <ThumbsUp/>Aplaudir <span>20</span>
+           </button>
           </footer>
          </div>
            </div>
