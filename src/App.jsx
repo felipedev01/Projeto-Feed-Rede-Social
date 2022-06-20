3a00e4377f821bf293df71fa8b0a4ded8a80c9fb
Felipe <felipecss88@gmail.com>
2022-06-20 07:40:14 -0300

    Criado arrays com objetos com informações pra post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,42 @@ import './Global.css'
 import Styles from './App.module.css'
 import {Sidebar} from './Componentes/Sidebar.jsx'
 
+const posts= [
+  {
+    id:1,
+    author:{
+            authorAvatar:"https://github.com/diego3g.png",
+            authorName:"Felipe Cristovão da Silva",
+            authorRole:"Web Developer"
+           },
+    publishedAt:new Date('2022-06-20 07:00:00'),
+
+    content:[{
+      type:'paragraph',
+      content:'Fala galeraa 👋',
+    },
+    {
+      type:'paragraph',
+      content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+
+    },
+    {
+      type:'link',
+      content:'👉 jane.design/doctorcare',
+    },
+    {
+      type:'link',
+      content:'#novoprojeto #nlw #rocketseat'
+    }
+    
+
+  ]
+
+  
+  }
+]
+
+
 export function App() {
 
   return(
