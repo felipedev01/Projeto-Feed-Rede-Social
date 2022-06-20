d099cb93d332ef11f1361addc9fee1dc086f452f
Felipe <felipecss88@gmail.com>
2022-06-20 08:02:20 -0300

    Post sendo reproduzido com map

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,38 @@ const posts= [
   ]
 
   
+  },
+  {
+    id:1,
+    author:{
+            authorAvatar:"https://github.com/diego3g.png",
+            authorName:"Felipe Cristovão da Silva",
+            authorRole:"Web Developer"
+           },
+    publishedAt:new Date('2022-06-20 07:00:00'),
+
+    content:[{
+      type:'paragraph',
+      content:'Fala galeraa 👋',
+    },
+    {
+      type:'paragraph',
+      content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+
+    },
+    {
+      type:'link',
+      content:'👉 jane.design/doctorcare',
+    },
+    {
+      type:'link',
+      content:'#novoprojeto #nlw #rocketseat'
+    }
+    
+
+  ]
+
+  
   }
 ]
 
@@ -52,20 +84,13 @@ export function App() {
      <div className={Styles.Wrapper}>
      <Sidebar></Sidebar>
        <main>
-       <Post 
-        author="Felipe" 
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nemo dolor sed nostrum recusandae quod repudiandae hic maiores, ipsa commodi sit quia blanditiis totam pariatur excepturi cumque, alias mollitia quidem.">
-       
-      </Post>
+       {posts.map(post =>{
+        return(
+             <Post></Post>
+        )
+       })
       
-      <Post 
-        author="Felipe" 
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nemo dolor sed nostrum recusandae quod repudiandae hic maiores, ipsa commodi sit quia blanditiis totam pariatur excepturi cumque, alias mollitia quidem.">
-
-       
-      </Post>
-      
-     
+      }
 
        </main>
      </div>
