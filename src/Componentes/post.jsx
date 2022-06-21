194dbaed1a256442ab0db71019dee50a60603ac2
Felipe <felipecss88@gmail.com>
2022-06-21 17:22:19 -0300

    Recebimento de propriedades com desestruturação

--- src/Componentes/post.jsx
+++ src/Componentes/post.jsx
@@ -1,9 +1,9 @@
  import styles from './post.module.css'
  import {Comment} from'./Comment.jsx'
  import {Avatar} from'./Avatar.jsx'
- export function Post(props){
+ export function Post({author,publishedAt,content}){
 
-    console.log(props.content)
+    //console.log({author})
 
     return (
         <article className={styles.post}>
@@ -11,26 +11,26 @@
            <header>
             <div className={styles.author}>
               <div className={styles.divAvatar}>
-              <Avatar  src={props.author.authorAvatar}></Avatar>
+              <Avatar  src={author.authorAvatar}></Avatar>
               </div>
             
             <div className={styles.authorinfo}>
-              <strong >{props.author.authorName}</strong>
-              <span>{props.author.authorRole}</span>
+              <strong >{author.authorName}</strong>
+              <span>{author.authorRole}</span>
             </div>
             </div>
-            <time title={props.publishedAt}>Publicado há 1h</time>
+            <time title='publishedAt'>Publicado há 1h</time>
            </header>
 
            <div className={styles.content}>
-            <p>{props.content[0].content}</p>
+            <p>{content[0].content}</p>
             <br></br>
-            <p>{props.content[1].content}</p>
+            <p>{content[1].content}</p>
              <br />
-           <p><a href="http://">{props.content[2].content}</a> </p>
+           <p><a href="http://">{content[2].content}</a> </p>
               <br />
             <p>
-             <a href="http://">{props.content[3].content}</a> </p>
+             <a href="http://">{content[3].content}</a> </p>
            </div>
            <form className={styles.CommentForm}>
             <strong>Deixe seu feedback</strong>
