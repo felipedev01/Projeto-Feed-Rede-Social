65e6d5a1ee96d14744580aa60b64a98beb46d114
Felipe <felipecss88@gmail.com>
2022-06-30 21:29:16 -0300

    Icone ignite carregado online

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -8,8 +8,8 @@ export function Header(){
     return(
     <div>
       <header className={styles.Header}> 
-        <img src="/src/Assets/Ignite-logo.svg" alt="Logo Ignite" />
-        <h1> Feed</h1>
+        <img src="https://ik.imagekit.io/Ro476mas/Ignite-logo_WKVNSckZo.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1656635132225" alt="Logo Ignite" />
+        <h1>Feed</h1>
         </header>
     </div>
         
